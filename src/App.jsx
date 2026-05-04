f9172a9033cbe85713a6b7eb7ff68fd6ac7828e7
Shalbin MS <shalbinms88@gmail.com>
2026-05-04 11:35:23 +0530

    Added API fetch and displayed player data with loading and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useEffect } from "react";
+
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [players , setPlayers] = useState([]);
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState(null);
+
+ 
   useEffect(() => {
     const fetchPlayers = async () => {
       try {
@@ -12,7 +18,7 @@ function App() {
           {
             method: "GET",
             headers: {
-              "X-RapidAPI-Key": "ac670c0601msh6a7fcfb0d2cd518p1ae736jsnde1c84388255", // replace this
+              "X-RapidAPI-Key": "ac670c0601msh6a7fcfb0d2cd518p1ae736jsnde1c84388255", 
               "X-RapidAPI-Host": "free-api-live-football-data.p.rapidapi.com"
             }
           }
@@ -24,18 +30,44 @@ function App() {
 
         console.log("3️⃣ Data parsed successfully:", data);
 
-        console.log("4️⃣ Players array:", data.response);
+        console.log("4️⃣ Players array:", data.response.suggestions);
 
+        setPlayers(data.response.suggestions);
+        setLoading(false);
 
       } catch (error) {
         console.log("Error:", error);
+        setError("Failed to fetch data");
+        setLoading(false);
       }
     };
 
     fetchPlayers();
   }, []);
 
-  return <h1>Football API Practice</h1>;
+  useEffect ( () => {
+    console.log("Updated players:", players);
+  },[players]);
+
+if (loading) {
+  return <h2>Loading players...</h2>;
+}
+
+if (error) {
+  return <h2>{error}</h2>;
+}
+return (
+  <div>
+    <h1>Football Players of the World</h1>
+
+    {players.map((player) => (
+      <div key={player.id}>
+        <p>{player.name}</p>
+      </div>
+    ))}
+
+  </div>
+);
 }
 
 export default App;
